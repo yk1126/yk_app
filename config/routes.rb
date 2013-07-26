69ddf7372d4042b83144c8639367216846dd1445
Yedhu Krishnan V P <yedhu@outlook.com>
2013-07-26 12:04:17 +0530

    Final Commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 YkApp::Application.routes.draw do
+  resources :users
+  resources :microposts
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
